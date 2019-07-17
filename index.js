46a11df832c1642edb10798304f7abd9d39d4c39
Angel Alanis <a4alaniz@gmail.com>
2019-07-17 01:22:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase("hello")
 }
-function logshout(string) {
-  console.log(.toUpperCase)
+function logShout(string){
+  return console.log(string.toUpperCase())
 }
+
+ logShout("hello")
+
+ function whisper(string){
+  return string.toLowerCase();
+}
+
+ whisper("HELLO")
+
+ function logWhisper(string){
+  return console.log(string.toLowerCase())
+}
+
+ logWhisper("HELLO")
+
+ function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) return "I can't hear you!";
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+  else if (string === string.toUpperCase()) return "YES INDEED!";
+}
+
+ sayHiToGrandma("hello");
+sayHiToGrandma("HELLO");
+sayHiToGrandma("I love you, Grandma.");
